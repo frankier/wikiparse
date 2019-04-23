0db765ed8e199d575a7d0226875aadd3c752f1ae
Frankie Robertson <frankie@robertson.name>
2019-04-23 19:25:28 +0300

    Put merged cmd in wikiparse.cmd

--- parse.py
+++ parse.py
@@ -1,14 +1,8 @@
-import click
-from wikiparse.cmd.db import db as db_group
-from wikiparse.cmd.parse import parse as parse_group
-from wikiparse.cmd.stats import stats as stats_group
+from wikiparse.cmd import merged
 import click_log
 
 click_log.basic_config()
 
 
-parse = click.CommandCollection(sources=[db_group, parse_group, stats_group])
-
-
 if __name__ == "__main__":
-    parse()
+    merged()
---
+++ wikiparse/cmd/__init__.py
@@ -0,0 +1,7 @@
+import click
+from wikiparse.cmd.db import db as db_group
+from wikiparse.cmd.parse import parse as parse_group
+from wikiparse.cmd.stats import stats as stats_group
+
+
+merged = click.CommandCollection(sources=[db_group, parse_group, stats_group], help="Commands for Wiktionary parsing and ETL")
