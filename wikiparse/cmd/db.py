6f64ebd37fbc95b5245d78e59d0af23e841efb90
Frankie Robertson <frankie@robertson.name>
2019-04-24 10:02:37 +0300

    Add trunc command

--- wikiparse/cmd/db.py
+++ wikiparse/cmd/db.py
@@ -14,3 +14,13 @@ def db():
 def create():
     session = get_session()
     metadata.create_all(session().get_bind().engine)
+
+
+@db.command()
+def trunc():
+    session = get_session()
+
+    for t in reversed(metadata.sorted_tables):
+        print("Dropping", t.name)
+        session.execute(f"TRUNCATE {t.name} RESTART IDENTITY CASCADE;")
+    session.commit()
