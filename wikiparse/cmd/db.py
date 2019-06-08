d61cfff97625ef26cbb76f070e179f2e646ccc4c
Frankie Robertson <frankie@robertson.name>
2019-06-08 08:27:07 +0300

    Factor out db commands into generic mk_db helper

--- wikiparse/cmd/db.py
+++ wikiparse/cmd/db.py
@@ -1,35 +1,5 @@
-import click
-import click_log
 from wikiparse.tables import metadata
-from wikiparse.utils.db import get_session
+from .mk_db import mk_cmds
 
 
-@click.group()
-@click_log.simple_verbosity_option()
-def db():
-    pass
-
-
-@db.command()
-def create():
-    session = get_session()
-    metadata.create_all(session().get_bind().engine)
-
-
-@db.command()
-def recreate():
-    session = get_session()
-    engine = session().get_bind().engine
-    metadata.reflect(bind=engine)
-    metadata.drop_all(engine)
-    metadata.create_all(engine)
-
-
-@db.command()
-def trunc():
-    session = get_session()
-
-    for t in reversed(metadata.sorted_tables):
-        print("Dropping", t.name)
-        session.execute(f"TRUNCATE {t.name} RESTART IDENTITY CASCADE;")
-    session.commit()
+db = mk_cmds(metadata)
